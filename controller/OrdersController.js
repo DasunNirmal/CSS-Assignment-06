380e0c142e375f54f866c7f6bb4d93049f0a4100
DasunNirmal <dasun200301@gmail.com>
2024-08-26 16:29:38 +0530

    Updated

--- controller/OrdersController.js
+++ controller/OrdersController.js
@@ -173,6 +173,7 @@ function ClearTwo() {
     $('#txtSearch-02').val("");
 }
 
+/*Search Customers*/
 function searchCustomers(query) {
     const searchTerm = query.toLowerCase(); /*Convert the search query to lowercase for case-insensitive search*/
 
@@ -461,7 +462,6 @@ $('#place-order').on('click', function () {
     defaultBorderColor();
     totalTagUpdate();
     loadOrderTable();
-    loadItemTable();
     loadOrderTableHome();
     updatePriceTag(); /*call this method to update price-tag if that same customer place another order*/
     ClearAll();
